58a90c482d252ee59c38b417655d8a0fdd8e497e
sesi200 <sev-sif@lula.ch>
2016-11-01 13:11:01 +0100

    Fixed non existing servlet

--- src/ch/uzh/ifi/climateapp/client/ClimateApp.java
+++ src/ch/uzh/ifi/climateapp/client/ClimateApp.java
@@ -1,15 +1,21 @@
 package ch.uzh.ifi.climateapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ClimateApp implements EntryPoint {
+	
+	private FlexTable tbl = new FlexTable();
 
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
+		tbl.setText(0, 0, "hello");
+		RootPanel.get("climateapp").add(tbl);
 		
 	}
 	
