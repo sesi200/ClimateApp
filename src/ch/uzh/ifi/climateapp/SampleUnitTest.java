7079f9766ad7d86843743f4f27524b1199ce96e4
sesi <sesi@sesis-MacBook-Pro.local>
2016-11-13 16:22:20 +0100

    made sample unit test file

---
+++ src/ch/uzh/ifi/climateapp/SampleUnitTest.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.climateapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
