9abc97c8522f67a9fdab46a286ca7f37b0346ac4
LadaM <lada11lada@gmail.com>
2016-11-14 12:55:04 +0100

    Deleted Initializer class; Added test for CSVReader

--- src/ch/uzh/ifi/climateapp/server/MyContextListener.java
+++ src/ch/uzh/ifi/climateapp/server/MyContextListener.java
@@ -12,56 +12,70 @@ import javax.servlet.ServletContextListener;
 import ch.uzh.ifi.climateapp.shared.ClimateData;
 
 public class MyContextListener implements ServletContextListener {
-	
+
 	private static ServletContext context;
 
-    /** 
-     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
-     * 
-     * This method initializes the CSVReader and calls the method to read the data from the .csv file
-     * List of climate data objects is created and filled with the data objects generated from .csv file
-     * The list is made unmodifiable, since climate data is not supposed to be added or deleted in this application
-     * Finally the list is put into the context of the application
-     */
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-    	context = sce.getServletContext();
-    	System.out.println("Context loaded!");
-        CSVReader myCsvReader = CSVReader.getInstance();
-        List<ClimateData> climateData = new ArrayList<ClimateData>();
-    	
-        System.out.println("Starting to read the CSV file");
-        long before = System.currentTimeMillis();
-    	try {
+	/**
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 * 
+	 *      This method initializes the CSVReader and calls the method to read
+	 *      the data from the .csv file List of climate data objects is created
+	 *      and filled with the data objects generated from .csv file The list
+	 *      is made unmodifiable, since climate data is not supposed to be added
+	 *      or deleted in this application Finally the list is put into the
+	 *      context of the application
+	 * 
+	 * @author lada
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		// getting the context and assigning it to the field of special
+		// singleton holder class
+		context = sce.getServletContext();
+		ServletContextHolder.getInstance(context);
+
+		System.out.println("Context loaded!");
+
+		CSVReader myCsvReader = CSVReader.getInstance();
+		List<ClimateData> climateData = new ArrayList<ClimateData>();
+
+		System.out.println("Starting to read the CSV file");
+		long before = System.currentTimeMillis();
+
+		try {
 			climateData = myCsvReader.readWithCsvBeanReader();
 		} catch (IOException e) {
 			System.out.println("The CSVReader has thrown an exception.");
 			e.printStackTrace();
 		}
-    	
-    	climateData = Collections.unmodifiableList(climateData);
-    	long timeTaken = System.currentTimeMillis() - before;
-    	System.out.println("Loaded " + climateData.size() + " climate data entries in " + timeTaken + "ms.");
-    	
-    	sce.getServletContext().setAttribute("climateData", climateData);
-  
-    	//test for getting the data out of the context
-    	
-//    	List<ClimateDataBean> list = (List<ClimateDataBean>) sce.getServletContext().getAttribute("climateData");
-//    	for (int i=0; i < 10000; i+=100){
-//    		ClimateDataBean data = list.get(i);
-//    		System.out.println(String.format("Date: %s, Country: %s, City: %s, Temperature: %f", 
-//    				data.getDt().toString(), data.getCountry(), data.getCity(), data.getAverageTemperature()));
-//    	}
-    }
 
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        // shutdown code here
-    }
-    
-    public static ServletContext getContext() {
-    	return context;
-    }
+		climateData = Collections.unmodifiableList(climateData);
+		long timeTaken = System.currentTimeMillis() - before;
+		System.out.println("Loaded " + climateData.size() + " climate data entries in " + timeTaken + "ms.");
+
+		sce.getServletContext().setAttribute("climateData", climateData);
+
+		// test for getting the data out of the context
+
+		// List<ClimateDataBean> list = (List<ClimateDataBean>)
+		// sce.getServletContext().getAttribute("climateData");
+		// for (int i=0; i < 10000; i+=100){
+		// ClimateDataBean data = list.get(i);
+		// System.out.println(String.format("Date: %s, Country: %s, City: %s,
+		// Temperature: %f",
+		// data.getDt().toString(), data.getCountry(), data.getCity(),
+		// data.getAverageTemperature()));
+		// }
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// shutdown code here
+	}
+
+	public static ServletContext getContext() {
+		return context;
+	}
 
 }
