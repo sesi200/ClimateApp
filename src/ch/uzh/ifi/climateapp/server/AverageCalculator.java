7949da70a5617a6ef202165a82d7f55147c578f5
LadaM <lada11lada@gmail.com>
2016-11-28 12:40:58 +0100

    Class for calculating averages per year and country

---
+++ src/ch/uzh/ifi/climateapp/server/AverageCalculator.java
@@ -0,0 +1,157 @@
+package ch.uzh.ifi.climateapp.server;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import ch.uzh.ifi.climateapp.shared.AverageData;
+import ch.uzh.ifi.climateapp.shared.ClimateData;
+
+/**
+ * Class is used to store all the methods for grouping and calculating 
+ * the average temperature per year and country. The result of this calculations 
+ * will be used to provide the data for the map representation.
+ * 
+ * @author lada
+ *
+ */
+public class AverageCalculator {
+	private final DecimalFormat formatter = new DecimalFormat("#.00");
+
+	/**
+	 * Method calculates the average temperature per country and year
+	 * and groups the resulting AverageData by year
+	 * 
+	 * @param list of ClimateData
+	 * @return map from year to AverageData
+	 */
+	public Map<Integer, List<AverageData>> calculateAveragePerYearAndCountry(List<ClimateData> list){
+		
+		Map<CountryYear, List<Double>> countryYearToList = groupByCountryYear(list);
+		Map<CountryYear, Double> countryYearToAverage = aggregateByCountryYear(countryYearToList);
+		
+		return groupPerYear(countryYearToAverage);
+	}
+
+
+	/**
+	 * Method gets the map from the CountryYear to average temperature and 
+	 * for each entry in the map creates new AverageData object.
+	 * AverageData objects are grouped by year in the resulting map.
+	 * 
+	 * @param countryYearToAverage
+	 * @return map from year to AverageData
+	 */
+	private Map<Integer, List<AverageData>> groupPerYear(Map<CountryYear, Double> countryYearToAverage) {
+		
+		Map<Integer,List<AverageData>> result = new LinkedHashMap<Integer, List<AverageData>>();
+		
+		for (Map.Entry<CountryYear, Double> entry : countryYearToAverage.entrySet()){
+			String country = entry.getKey().getCountry();
+			String avgTemp = roundAndToString(entry.getValue()); //as String to enable the output on the map
+			int year = entry.getKey().getYear();
+			AverageData averageData = new AverageData(country, avgTemp, year);
+			
+			if(!result.containsKey(entry.getKey().getYear())){
+				result.put(entry.getKey().getYear(), new ArrayList<AverageData>());
+			}
+			result.get(entry.getKey().getYear()).add(averageData);
+		}
+		return result;
+	}
+	
+	/**
+	 * Method rounds the double to two digits after decimal point and 
+	 * transforms it to string (to be able to output more precise temperature on the map)
+	 * 
+	 * @param value
+	 * @return String representation of the value with 2 digits after decimal point
+	 */
+	private String roundAndToString(Double value) {		
+		double rounded = round(value, 2);
+		return formatter.format(rounded);
+	}
+	
+	/**
+	 * Method transforms the multimap with the list as a value 
+	 * to map with average of that list. The key stays the same.
+	 * 
+	 * @param countryYearToList
+	 * @return map from CountryYear to average temperature per country and year
+	 */
+	private Map<CountryYear, Double> aggregateByCountryYear(
+			Map<CountryYear, List<Double>> countryYearToList) {
+		 
+		Map<CountryYear, Double> result = new LinkedHashMap<CountryYear, Double>();
+		for(Map.Entry<CountryYear, List<Double>> entry : countryYearToList.entrySet()){
+			double average = calculateAverage(entry.getValue());
+			result.put(entry.getKey(), average);
+		}
+		return result;
+	}
+
+	/**
+	 * @param list of temperatures
+	 * @return average
+	 */
+	private double calculateAverage(List<Double> list) {
+		double sum = 0d;
+		for(double value : list){
+			sum += value;
+		}
+		return sum/list.size();
+	}
+
+	/**
+	 * Method groups the original data (ClimateData) by country and year, 
+	 * using the CountryYear object as key.
+	 * @param list of ClimateData
+	 * @return multimap from CountryYear to list of temperatures
+	 */
+	private Map<CountryYear, List<Double>> groupByCountryYear(List<ClimateData> list) {
+		
+		Map<CountryYear, List<Double>> result = new LinkedHashMap<CountryYear, List<Double>>();
+		for (ClimateData climateData : list){
+			int year = getYear(climateData.getDt());
+			String country = climateData.getCountry();
+			CountryYear countryYear = new CountryYear(country, year);
+			
+			if(!result.containsKey(countryYear)){
+				result.put(countryYear, new ArrayList<Double>());
+			} 
+			result.get(countryYear).add(climateData.getAverageTemperature());
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Method extracts year from the date and returns the correct integer representation of it
+	 * @param date
+	 * @return the integer value of a year
+	 */
+	@SuppressWarnings("deprecation")
+	private int getYear(Date date){
+		return (date.getYear() + 1900);
+	}
+
+	
+	/**
+	 * @param value
+	 * @param places
+	 * @return rounded value
+	 */
+	private double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+	    
+	    long factor = (long) Math.pow(10, places);
+	    value = value * factor;
+	    long tmp = Math.round(value);
+	    return (double) tmp / factor;
+	}
+}
---
+++ src/ch/uzh/ifi/climateapp/server/AverageCalculatorTest.java
@@ -0,0 +1,87 @@
+package ch.uzh.ifi.climateapp.server;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ch.uzh.ifi.climateapp.shared.AverageData;
+import ch.uzh.ifi.climateapp.shared.ClimateData;
+
+public class AverageCalculatorTest {
+	// class under test
+	private final AverageCalculator calc = new AverageCalculator();
+
+	@Test
+	public void testCalculateAveragePerYearAndCountry_grouped() {
+		// given
+		List<ClimateData> climateDatas = Arrays.asList(
+				new ClimateData(createDate(2001), 20, 0d, "Zürich", "Switzerland", "", ""),
+				new ClimateData(createDate(2001), 21, 0d, "Geneva", "Switzerland", "", ""),
+				new ClimateData(createDate(2000), 21, 0d, "Geneva", "Switzerland", "", ""),
+				new ClimateData(createDate(2000), 21, 0d, "Seattle", "USA", "", ""),
+				new ClimateData(createDate(2001), 21, 0d, "Seattle", "USA", "", "")
+				);
+				
+		// do
+		Map<Integer, List<AverageData>> actual = calc.calculateAveragePerYearAndCountry(climateDatas);
+		
+		// verify
+		Map<Integer, List<AverageData>> expected = new LinkedHashMap<Integer, List<AverageData>>();
+		expected.put(2001, Arrays.asList(
+				new AverageData("Switzerland", "20.50", 2001),
+				new AverageData("USA", "21.00", 2001)
+				));
+		expected.put(2000, Arrays.asList(
+				new AverageData("Switzerland", "21.00", 2000),
+				new AverageData("USA", "21.00", 2000)));
+		
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCalculateAveragePerYearAndCountry_emptyInput_emptyOutput(){
+		//given
+		List<ClimateData> climateDatas = Collections.emptyList();
+		
+		//do
+		Map<Integer, List<AverageData>> result = calc.calculateAveragePerYearAndCountry(climateDatas);
+		
+		//verify
+		Assert.assertTrue(result.isEmpty());
+	}
+	@Test
+	public void testCalculateAveragePerYearAndCountry_rounded() {
+		// given
+		List<ClimateData> climateDatas = Arrays.asList(
+				new ClimateData(createDate(2001), 1.505, 0d, "", "CH", "", ""),
+				new ClimateData(createDate(2001), 1.504, 0d, "", "UA", "", "")
+				);
+				
+		// do
+		Map<Integer, List<AverageData>> actual = calc.calculateAveragePerYearAndCountry(climateDatas);
+		
+		// verify
+		Map<Integer, List<AverageData>> expected = new LinkedHashMap<Integer, List<AverageData>>();
+		expected.put(2001, Arrays.asList(
+				new AverageData("CH", "1.51", 2001),
+				new AverageData("UA", "1.50", 2001)
+				));
+		assertEquals(expected, actual);
+	}
+	
+	@SuppressWarnings("deprecation")
+	private Date createDate(int year) {
+		return new Date(year - 1900, 1, 1);
+	}
+
+}
