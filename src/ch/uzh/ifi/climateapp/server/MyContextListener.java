7bf5b72e3dddf89a239026a539a6623850b751b5
LadaM <lada11lada@gmail.com>
2016-12-09 14:06:16 +0100

    Creating arrays with country names and city names

--- src/ch/uzh/ifi/climateapp/server/MyContextListener.java
+++ src/ch/uzh/ifi/climateapp/server/MyContextListener.java
@@ -2,9 +2,12 @@ package ch.uzh.ifi.climateapp.server;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -30,10 +33,24 @@ public class MyContextListener implements ServletContextListener {
 	 * 
 	 * @author lada
 	 */
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 */
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 */
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 */
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 
 		AverageCalculator avgCalc = new AverageCalculator();
+		//sets for lookup and arrays to give the list with all country and city names
+		Set<String> countryNamesSet = new HashSet<String>();
+		Set<String> cityNamesSet = new HashSet<String>();
+		String[] countryNames;
+		String[] cityNames;
 
 		System.out.println("Context loaded!");
 
@@ -49,7 +66,32 @@ public class MyContextListener implements ServletContextListener {
 			System.out.println("The CSVReader has thrown an exception.");
 			e.printStackTrace();
 		}
-
+		
+		/**
+		 * Create lookup hashset for country names and city names
+		 * For each ClimateData add the country / city name to the set,
+		 * if not is not yet there
+		 */
+		for(ClimateData data : climateData){
+			String currCountName = data.getCountry();
+			String currCityName = data.getCity();
+			
+			if(!countryNamesSet.contains(currCountName)){
+				countryNamesSet.add(currCountName);
+			}
+			if (!cityNamesSet.contains(currCityName)){
+				cityNamesSet.add(currCityName);
+			}
+		}
+		
+		//transforming the sets to array and putting them to the context
+		countryNames = countryNamesSet.toArray(new String[countryNamesSet.size()]);
+		Arrays.sort(countryNames);		
+		cityNames = cityNamesSet.toArray(new String[cityNamesSet.size()]);
+		Arrays.sort(countryNames);
+		sce.getServletContext().setAttribute(ContextContent.COUNTRIES, countryNames);
+		sce.getServletContext().setAttribute(ContextContent.CITIES, cityNames);
+		
 		climateData = Collections.unmodifiableList(climateData);
 		long timeTaken = System.currentTimeMillis() - before;
 		System.out.println("Loaded " + climateData.size() + " climate data entries in " + timeTaken + "ms.");
@@ -60,14 +102,6 @@ public class MyContextListener implements ServletContextListener {
 		sce.getServletContext().setAttribute(ContextContent.CLIMATE_DATA, climateData);
 		sce.getServletContext().setAttribute(ContextContent.AVERAGE_PER_YEAR, averageDataMap);
 		
-		// test for getting the data out of the context
-
-//		 @SuppressWarnings("unchecked")
-//		 List<ClimateData> list = (List<ClimateData>) sce.getServletContext().getAttribute("climateData");
-//		 for (int i=0; i < 100; i++){
-//		 ClimateData data = list.get(i);
-//		 System.out.println(data);
-//		 }
 	}
 
 	@Override
