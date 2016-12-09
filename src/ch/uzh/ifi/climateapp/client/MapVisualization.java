0d4c736080f09bc96711bf39bd3ded9dbf406bcd
sesi <sesi@sesis-MacBook-Pro.local>
2016-12-09 13:52:54 +0100

    MapVis cleanup

--- src/ch/uzh/ifi/climateapp/client/MapVisualization.java
+++ src/ch/uzh/ifi/climateapp/client/MapVisualization.java
@@ -13,67 +13,17 @@ import ch.uzh.ifi.climateapp.shared.AverageData;
 public class MapVisualization implements IVisualization{
 	
 	
-	private int mapWidth = 900;
+	private int mapWidth = 1200;
 	private int mapHeight = 500;
 	
 	private DataTable dataTable;
 	private GeoMap.Options options;
-	private GeoMap geomap;
+	private GeoMap geomap = null;
 	
 	private AverageData[] averageData;
 	
 	
 	public MapVisualization(){}
-	
-	
-/*
-	public Widget getVisualization(final VerticalPanel verticalPanel, List<AverageData> averageForYear) {
-		this.averageForYear = averageForYear;
-		
-		Runnable onLoadCallback = new Runnable(){
-			
-			
-			/**
-			 * This method uses he gwt-visualization-1.1.2 libarary to visualize the climate data on to a map
-			 * 
-			 * @return visualized map widget
-			 */
-	
-	/*
-			@Override
-			public void run() {
-				// Building 
-				dataTable = DataTable.create();
-				dataTable.addColumn(ColumnType.STRING, "Country");
-				dataTable.addColumn(ColumnType.NUMBER, "Temperature");
-				dataTable.addRows(averageForYear.size());
-				
-				
-				for (int i = 0; i < averageForYear.size(); i++){
-					dataTable.setValue(i, 0, averageForYear.get(i).getCountry());
-					int avg = (int) Math.round(averageForYear.get(i).getAvgTemp());
-					dataTable.setValue(i, 1, avg);
-				}
-				
-				options = GeoMap.Options.create();
-				options.setDataMode(GeoMap.DataMode.REGIONS);
-				options.setRegion("world");
-				options.setWidth(mapWidth);
-				options.setHeight(mapHeight);
-				options.setShowLegend(true);
-				geomap = new GeoMap(dataTable, options);
-				
-				verticalPanel.clear();
-				verticalPanel.add(geomap);
-				
-			}
-		};
-		
-		VisualizationUtils.loadVisualizationApi(onLoadCallback, GeoMap.PACKAGE);
-		return verticalPanel;
-	}
-	
-	*/
 	
 	@Override
 	public Widget getVisualization(final VerticalPanel verticalPanel) {
@@ -99,16 +49,7 @@ public class MapVisualization implements IVisualization{
 					dataTable.setValue(i, 1, averageData[i].getAvgTemp());
 				}
 				
-				options = GeoMap.Options.create();
-				options.setDataMode(GeoMap.DataMode.REGIONS);
-				options.setRegion("world");
-				options.setWidth(mapWidth);
-				options.setHeight(mapHeight);
-				options.setShowLegend(true);
-				geomap = new GeoMap(dataTable, options);
 				
-				verticalPanel.clear();
-				verticalPanel.add(geomap);
 				
 				FlexTable selectedYear = new FlexTable();
 				int year = averageData[1].getYear();
@@ -116,7 +57,22 @@ public class MapVisualization implements IVisualization{
 				selectedYear.addStyleName("mapLabel");
 				String yearText = Integer.toString(year);
 				selectedYear.setText(0, 2, yearText);
-				verticalPanel.add(selectedYear);
+
+				if(verticalPanel.getWidgetCount()==0) {
+					if (geomap == null) {
+						options = GeoMap.Options.create();
+						options.setDataMode(GeoMap.DataMode.REGIONS);
+						options.setRegion("world");
+						options.setWidth(mapWidth);
+						options.setHeight(mapHeight);
+						options.setShowLegend(true);
+						geomap = new GeoMap(dataTable, options);
+					}
+					verticalPanel.add(selectedYear);
+					verticalPanel.add(geomap);
+				}
+				geomap.draw(dataTable);
+				
 				
 			}
 		};
